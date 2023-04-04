4437edf4953d954ec27a8a0eea69c12e89620623
valdirssilva <valdir.soares.da.silva25@gmail.com>
2023-04-03 22:21:15 -0300

    lista

--- TSP_julia/src/TSP.jl
+++ TSP_julia/src/TSP.jl
@@ -1,5 +1,36 @@
-module TSP
+push!(LOAD_PATH, "src/modules/")
 
-greet() = print("Hello World!")
+using Pkg
+Pkg.activate(".")
 
-end # module TSP
+using JuMP
+using Gurobi
+using DelimitedFiles
+
+using Data
+#using OutputStatistics
+#using Parameters
+#using Formulation
+
+# To run the code: julia TSP.jl <inputListFile> 
+#                                         [1]             
+#inputListFile:   inputFiles/<all.txt> or <> or <>
+
+# Read input list file
+input = readdlm(String(ARGS[1]))
+
+# Get number of instances to run
+numInst = input[1]
+
+# Run all numInst instances in the input list file
+for inst = 1:numInst
+
+    # Get the name of the file containing the instance data
+    instanceFile = String(input[inst + 1])
+
+    println(instanceFile)
+    # Read instance data
+    #data = Data.readData(instanceFile)
+
+
+end
--- TSP_julia/src/modules/Parameters/src/Parameters.jl
+++ TSP_julia/src/modules/Parameters/src/Parameters.jl
@@ -1,6 +1,6 @@
 module Parameters
 
-using Data
+
 using DelimitedFiles
 
 struct StdFormParameters
@@ -10,33 +10,9 @@ struct StdFormParameters
     numberOfThreads::Int
     screenOutput::Int
 end
-struct RFFOParameters
-    rfMaxTime::Int
-    restrictedModelMaxTime::Float64
-    rfWindowSize::Int
-    rfStep::Int
-    foWindowSize::Int
-    foStep::Int
-    MIPgapTolerance::Float64
-    integerFeasibilityTolerance::Float64
-    numberOfThreads::Int
-    screenOutput::Int
-end
-struct RFOParameters
-    rfMaxTime::Int
-    restrictedModelMaxTime::Float64
-    rfWindowSize::Int
-    rfStep::Int
-    foWindowSize::Int
-    foStep::Int
-    MIPgapTolerance::Float64
-    integerFeasibilityTolerance::Float64
-    numberOfThreads::Int
-    screenOutput::Int
-    startFO:: Int
-end
 
-export StdFormParameters, readStdFormParameters,RFFOParameters, readRFFOParameters,RFOParameters, readRFOParameters
+
+export StdFormParameters, readStdFormParameters
 
 function readStdFormParameters(paramFile::String)
     paramData = readdlm(paramFile)
@@ -55,64 +31,6 @@ function readStdFormParameters(paramFile::String)
     return params
 end
 
-function readRFFOParameters(paramFile::String, data::InstanceData)
-    paramData = readdlm(paramFile)
-
-    rfMaxTime = paramData[1,2]
-    restrictedModelMaxTime = paramData[2,2]  #paramData[1,2]/ (2 * ceil(1 + (data.numPer - paramData[7,2] ) / paramData[8,2])) 
-    rfWindowSize =  paramData[7,2] #ceil(data.numPer / 3)  ### restrictedModelMaxTime = rfMaxTime / (2 * arrendondamento(1+(m-tj)/tp))
-    rfStep = paramData[8,2] #ceil((2 * rfWindowSize) / 3) 
-    foWindowSize = paramData[9,2] #ceil(data.numPer / 3)
-    foStep = paramData[10,2] #ceil(foWindowSize / 2)
-
-    MIPgapTolerance = paramData[3,2]
-    integerFeasibilityTolerance = paramData[4,2]
-    numberOfThreads = paramData[5,2]
-    screenOutput = paramData[6,2]
-
-    params = RFFOParameters(rfMaxTime,
-                            restrictedModelMaxTime,
-                            rfWindowSize,
-                            rfStep,
-                            foWindowSize,
-                            foStep,
-                            MIPgapTolerance,
-                            integerFeasibilityTolerance,
-                            numberOfThreads,
-                            screenOutput)
-
-    return params
-end
-function readRFOParameters(paramFile::String, data::InstanceData)
-    paramData = readdlm(paramFile)
-
-    rfMaxTime = paramData[1,2]
-    restrictedModelMaxTime = paramData[1,2]/ (2 * ceil(1 + (data.numPer - paramData[7,2] ) / paramData[8,2])) #paramData[2,2] 
-    rfWindowSize = paramData[7,2]#ceil(data.numPer / 3)
-    rfStep = paramData[8,2] #ceil((2 * rfWindowSize) / 3)
-    foWindowSize = paramData[9,2] #ceil(data.numPer / 3)
-    foStep = paramData[10,2] #ceil(foWindowSize / 2)
-    startFO = paramData[11,2]
-
-    MIPgapTolerance = paramData[3,2]
-    integerFeasibilityTolerance = paramData[4,2]
-    numberOfThreads = paramData[5,2]
-    screenOutput = paramData[6,2]
-
-    params = RFOParameters(rfMaxTime,
-                            restrictedModelMaxTime,
-                            rfWindowSize,
-                            rfStep,
-                            foWindowSize,
-                            foStep,
-                            MIPgapTolerance,
-                            integerFeasibilityTolerance,
-                            numberOfThreads,
-                            screenOutput,
-                            startFO)
-
-    return params
-end
 
 
 end
